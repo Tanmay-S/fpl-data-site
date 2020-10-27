698847d1bc1129367479f068cea7249985fa541b
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-10-28 03:12:32 +0530

    Initial commit

---
+++ src/main/java/com/ahc/fpldatasite/FplDataSiteApplication.java
@@ -0,0 +1,13 @@
+package com.ahc.fpldatasite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplDataSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FplDataSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahc/fpldatasite/FplDataSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahc.fpldatasite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FplDataSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
