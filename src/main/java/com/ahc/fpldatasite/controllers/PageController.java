76433186aacd1045a195d2076d08c5c07a2d2c69
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-10-28 14:27:54 +0530

    Index Page added

---
+++ src/main/java/com/ahc/fpldatasite/controllers/PageController.java
@@ -0,0 +1,13 @@
+package com.ahc.fpldatasite.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping({"", "/"})
+    public String getIndex() {
+        return "index";
+    }
+}
