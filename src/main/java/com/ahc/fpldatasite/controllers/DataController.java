95affbef4858c761d589df75adb172b49a3eda33
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-10-29 15:50:57 +0530

    Refactoring and moving to Data directory

--- src/main/java/com/ahc/fpldatasite/controllers/DataController.java
+++ src/main/java/com/ahc/fpldatasite/controllers/DataController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class PageController {
+@RequestMapping("/data")
+public class DataController {
 
     @RequestMapping({"", "/"})
-    public String getIndex() {
-        return "index";
+    public String getIndexPage() {
+        return "data/index";
     }
 }
