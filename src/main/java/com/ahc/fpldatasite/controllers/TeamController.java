92861f007304e02d37faae58e498554f3c8f85e3
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-10-31 17:20:17 +0530

    Added more Team's stats pages.

--- src/main/java/com/ahc/fpldatasite/controllers/TeamController.java
+++ src/main/java/com/ahc/fpldatasite/controllers/TeamController.java
@@ -23,6 +23,83 @@ public class TeamController {
         return "team/index";
     }
 
+    @RequestMapping("fantasy")
+    public String getFantasyStats(Model model) {
+        try {
+            String table = tableParserService.getTable(FfsUrls.TEAM_FANTASY.url());
+            model.addAttribute("fantasyTable", table);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return "team/fantasy";
+    }
+
+    @RequestMapping("involvement")
+    public String getInvolvementStats(Model model) {
+        try {
+            String table = tableParserService.getTable(FfsUrls.TEAM_INVOLVEMENT.url());
+            model.addAttribute("involvementTable", table);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return "team/involvement";
+    }
+
+    @RequestMapping("distribution")
+    public String getDistributionStats(Model model) {
+        try {
+            String table = tableParserService.getTable(FfsUrls.TEAM_DISTRIBUTION.url());
+            model.addAttribute("distributionTable", table);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return "team/distribution";
+    }
+
+    @RequestMapping("threat")
+    public String getThreatStats(Model model) {
+        try {
+            String table = tableParserService.getTable(FfsUrls.TEAM_THREAT.url());
+            model.addAttribute("threatTable", table);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return "team/threat";
+    }
+
+    @RequestMapping("defending")
+    public String getDefendingStats(Model model) {
+        try {
+            String table = tableParserService.getTable(FfsUrls.TEAM_DEFENDING.url());
+            model.addAttribute("defendingTable", table);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return "team/defending";
+    }
+
+    @RequestMapping("set-piece")
+    public String getSetPieceStats(Model model) {
+        try {
+            String table = tableParserService.getTable(FfsUrls.TEAM_SET_PIECE.url());
+            model.addAttribute("setPieceTable", table);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return "team/set-piece";
+    }
+
+    @RequestMapping("discipline")
+    public String getDisciplineStats(Model model) {
+        try {
+            String table = tableParserService.getTable(FfsUrls.TEAM_DISCIPLINE.url());
+            model.addAttribute("disciplineTable", table);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return "team/discipline";
+    }
+
     @RequestMapping("expected")
     public String getExpectedStats(Model model) {
         try {
