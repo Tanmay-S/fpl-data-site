692d1ed4a11df42c67fb2682c7df8da0fa24f78e
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-11-02 19:31:25 +0530

    Removed unused import

--- src/main/java/com/ahc/fpldatasite/FplDataSiteApplication.java
+++ src/main/java/com/ahc/fpldatasite/FplDataSiteApplication.java
@@ -1,16 +1,13 @@
 package com.ahc.fpldatasite;
 
-import com.ahc.fpldatasite.logic.FfsHtmlTableParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class FplDataSiteApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FplDataSiteApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FplDataSiteApplication.class, args);
+	}
 
 }
