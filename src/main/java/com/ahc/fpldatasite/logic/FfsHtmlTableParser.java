b50c84d008429cfa4ecc939b05064eccb6c4a599
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-11-03 00:40:43 +0000

    Changed indent from Tabs to Spaces.

--- src/main/java/com/ahc/fpldatasite/logic/FfsHtmlTableParser.java
+++ src/main/java/com/ahc/fpldatasite/logic/FfsHtmlTableParser.java
@@ -9,21 +9,21 @@ import java.io.IOException;
 
 public class FfsHtmlTableParser {
 
-	private static final String COOKIE_NAME = SecurityKeys.FFS_COOKIE_NAME.value();
-	private static final String FFS_TOKEN = SecurityKeys.FFS_TOKEN.value();
-	private final String url;
+    private static final String COOKIE_NAME = SecurityKeys.FFS_COOKIE_NAME.value();
+    private static final String FFS_TOKEN = SecurityKeys.FFS_TOKEN.value();
+    private final String url;
 
-	public FfsHtmlTableParser(String url) {
-		this.url = url;
-	}
+    public FfsHtmlTableParser(String url) {
+        this.url = url;
+    }
 
-	private Document setDocument() throws IOException {
-		return Jsoup.connect(url).cookie(COOKIE_NAME, FFS_TOKEN).get();
-	}
+    private Document setDocument() throws IOException {
+        return Jsoup.connect(url).cookie(COOKIE_NAME, FFS_TOKEN).get();
+    }
 
-	public String getTable() throws IOException {
-		Document document = setDocument();
-		Element tableElement = document.select("table").first();
-		return tableElement.outerHtml();
-	}
+    public String getTable() throws IOException {
+        Document document = setDocument();
+        Element tableElement = document.select("table").first();
+        return tableElement.outerHtml();
+    }
 }
