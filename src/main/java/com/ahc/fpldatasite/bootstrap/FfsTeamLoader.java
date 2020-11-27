d1a437c7dac83ba8f43d0046dbc87c4ed36d3eaa
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-11-28 03:38:22 +0530

    Added relationship between team and new fantasy table

--- src/main/java/com/ahc/fpldatasite/bootstrap/FfsTeamLoader.java
+++ src/main/java/com/ahc/fpldatasite/bootstrap/FfsTeamLoader.java
@@ -1,6 +1,7 @@
 package com.ahc.fpldatasite.bootstrap;
 
 import com.ahc.fpldatasite.constants.FfsUrls;
+import com.ahc.fpldatasite.domain.Fantasy;
 import com.ahc.fpldatasite.domain.Team;
 import com.ahc.fpldatasite.repositories.TeamRepository;
 import com.ahc.fpldatasite.services.TableParserService;
@@ -30,16 +31,21 @@ public class FfsTeamLoader {
 
             Team team = new Team();
             team.setName(tableData.next().text());
-            team.setGamesPlayed(Integer.valueOf(tableData.next().text()));
-            team.setGoals(Integer.valueOf(tableData.next().text()));
-            team.setAssists(Integer.valueOf(tableData.next().text()));
-            team.setFantasyAssists(Integer.valueOf(tableData.next().text()));
-            team.setTotalAssists(Integer.valueOf(tableData.next().text()));
-            team.setCleanSheets(Integer.valueOf(tableData.next().text()));
-            team.setGoalsConceded(Integer.valueOf(tableData.next().text()));
-            team.setOwnGoalsConceded(Integer.valueOf(tableData.next().text()));
-            team.setYellowCards(Integer.valueOf(tableData.next().text()));
-            team.setRedCards(Integer.valueOf(tableData.next().text()));
+
+            Fantasy fantasy = new Fantasy();
+            fantasy.setGamesPlayed(Integer.valueOf(tableData.next().text()));
+            fantasy.setGoals(Integer.valueOf(tableData.next().text()));
+            fantasy.setAssists(Integer.valueOf(tableData.next().text()));
+            fantasy.setFantasyAssists(Integer.valueOf(tableData.next().text()));
+            fantasy.setTotalAssists(Integer.valueOf(tableData.next().text()));
+            fantasy.setCleanSheets(Integer.valueOf(tableData.next().text()));
+            fantasy.setGoalsConceded(Integer.valueOf(tableData.next().text()));
+            fantasy.setOwnGoalsConceded(Integer.valueOf(tableData.next().text()));
+            fantasy.setYellowCards(Integer.valueOf(tableData.next().text()));
+            fantasy.setRedCards(Integer.valueOf(tableData.next().text()));
+
+            team.setFantasy(fantasy);
+            fantasy.setTeam(team);
 
             teamRepository.save(team);
         }
